81f6188b7eebf29f734639d14bec69a77db5c16d
Uspenskiy.Dmitriy <12gamedevowl21@gmail.com>
2024-04-06 11:54:11 +0400

    Add project files.

---
+++ ExDyslex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExDyslex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
