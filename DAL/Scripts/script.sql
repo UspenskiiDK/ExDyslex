df24195149c0c7f8532c0975567fad7de97510a4
Uspenskiy.Dmitriy <12gamedevowl21@gmail.com>
2024-05-26 14:31:58 +0400

    Добавил DAL и script

---
+++ DAL/Scripts/script.sql
@@ -0,0 +1,29 @@
+﻿create database ExDyslex;
+go
+
+use ExDyslex;
+go
+
+create table Users
+(
+	Id int not null identity constraint PK_Users primary key,
+	FirstName nvarchar(max) not null,
+	LastName nvarchar(max) not null,
+	PatronymicName nvarchar(max),
+	Birthday datetime,
+	[Password] nvarchar(max) not null,
+	RoleId int not null
+)
+
+create table Clients
+(
+	Id int not null identity constraint PK_Clients primary key,
+	FirstName nvarchar(max) not null,
+	LastName nvarchar(max),
+	PatronymicName nvarchar(max),
+	Birthday datetime,
+	Phone nvarchar(max),
+	Email nvarchar(max) not null,
+	[Password] nvarchar(max) not null,
+	MainPhotoPath nvarchar(max)
+)
